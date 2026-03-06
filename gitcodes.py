4c0e0dad7c2b4cc335f1a4681785245480498b8d
sumedhachowdary-coder <sumedha.challagundla2023@vitstudent.ac.in>
2026-03-06 14:21:32 +0530

    js

---
+++ gitcodes.py
@@ -0,0 +1,20 @@
+#git init
+#git add .
+#git commit -m "Initial commit"
+#git branch -M main
+#git remote add origin <url>
+#git push -u origin main
+#-------------------------
+#git remote -v
+#git remote set-url origin <url>
+#-------------------------
+#git checkout -b develop
+#git push -u origin develop
+#git checkout -b feature/login
+#git push -u origin feature/login
+#git checkout develop
+#git merge feature/login
+#git branch -d feature/login
+#git push origin --delete feature/login
+#git tag v1.0
+#git push --tags 
