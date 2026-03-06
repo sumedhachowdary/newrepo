71e6a9bab48ed4e600272c494e86170accbb8f8b
sumedhachowdary-coder <sumedha.challagundla2023@vitstudent.ac.in>
2026-03-06 13:41:20 +0530

    initial

---
+++ code3.py
@@ -0,0 +1,12 @@
+num1=22
+num2=33
+operator='+'
+if operator=='+':
+    result=num1+num2
+elif operator=='-':
+    result=num1-num2
+elif operator=='/':
+    result=num1/num2
+else:
+    result=num1*num2
+print(result)
